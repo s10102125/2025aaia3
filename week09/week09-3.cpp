65f5c20e418eda84447cec1c922da88774d824c6
s10102125 <128062151+s10102125@users.noreply.github.com>
2025-11-06 17:15:59 +0800

    week09

---
+++ week09/week09-3.cpp
@@ -0,0 +1,34 @@
+// week09-3.cpp 學習計畫 Matrix 第3題
+// 54. Spiral Matrix
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int M = matrix.size(), N = matrix[0].size(); // 先把上下左右的邊界，都宣告一個變數
+        int up = 0, down = M - 1, left = 0, right = N - 1; // 先把上下左右的邊界，都宣告一個變數
+        vector<int> ans;
+        while (up <= down && left <= right) {
+            for (int i = left; i <= right; i++) { // 從左到右
+                ans.push_back(matrix[up][i]);
+            }
+            up++; // AI說，要寫up++上屆內縮
+
+            for (int i = up; i <= down; i++) { // 往下
+                ans.push_back(matrix[i][right]);
+            }
+            right--; // AI say 右邊界內縮
+
+            if (!(up <= down && left <= right)) break; // 邊界超出範圍交叉，無法繼續
+
+            for (int i = right; i >= left; i--) { // (到過來的迴圈)往左
+                ans.push_back(matrix[down][i]);
+            }
+            down--;
+
+            for (int i = down; i >= up; i--) { // (到過來的迴圈) 往上
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+        return ans;
+    }
+};
